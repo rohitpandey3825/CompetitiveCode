aee6ee48abaaf482c67be2eb2c54f2944dc5e57a
ROHIT PANDEY <rohit.pandey3825@gmail.com>
2022-10-19 14:34:32 +0530

    genric main

--- Program/Program.cs
+++ Program/Program.cs
@@ -13,26 +13,7 @@ namespace Problem
             // ******************Code block******************
             try
             {
-                int T = Convert.ToInt32(Console.ReadLine());
-                for (int i = 0; i < T; i++)
-                {
-                    string origstr = Console.ReadLine();
-                    int N = Convert.ToInt32(origstr.Split(' ')[0]);
-                    int K = Convert.ToInt32(origstr.Split(' ')[1]);
-                    int k = K % N;
-                    if (k > 0)
-                    {
-                        string newstr = Console.ReadLine();
-                        string sp1 = newstr.Substring(0, 2 * (N - k) - 1);
-                        string sp2 = newstr.Substring(2 * (N - k));
-
-                        Console.WriteLine(sp2 + " " + sp1);
-                    }
-                    else
-                    {
-                        Console.WriteLine(origstr);
-                    }
-                }
+                MainSubstitute();
             }
             catch (Exception ex)
             {
@@ -43,6 +24,19 @@ namespace Problem
             finally
             {
                 Console.Dispose();
+            }
+        }
+
+        static void MainSubstitute()
+        {
+            int T = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < T; i++)
+            {
+                string origstr = Console.ReadLine();
+                int N = Convert.ToInt32(origstr.Split(' ')[0]);
+                int K = Convert.ToInt32(origstr.Split(' ')[1]);
+
+
             }
         }
     }
