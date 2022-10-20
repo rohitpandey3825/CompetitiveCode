599d36a0510cceb0658adcd1ab57551b283cf3df
ROHIT PANDEY <rohit.pandey3825@gmail.com>
2022-10-20 19:08:55 +0530

    Main changes for Desing Pattern

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,43 +1,14 @@
 ﻿using System;
-using Helper.IO;
+using Problem.Factory;
+using Problem.Interface;
 
 namespace Problem
 {
     class Program
     {
-        private static ConsoleReader Console = new ConsoleReader("Problem");
-
         static void Main(string[] args)
         {
-
-            // ******************Code block******************
-            try
-            {
-                MainSubstitute();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-            }
-            // ******************Copy till here******************
-            // ******************OR delete below lines******************
-            finally
-            {
-                Console.Dispose();
-            }
-        }
-
-        static void MainSubstitute()
-        {
-            int T = Convert.ToInt32(Console.ReadLine());
-            for (int i = 0; i < T; i++)
-            {
-                string origstr = Console.ReadLine();
-                int N = Convert.ToInt32(origstr.Split(' ')[0]);
-                int K = Convert.ToInt32(origstr.Split(' ')[1]);
-
-
-            }
+            
         }
     }
 }
